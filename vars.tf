0ae2bf8b62269645143299bbb9eb1ca4150b1ba9
stoffee <chris@roadkill.org>
2019-06-18 09:04:11 -0700

    adding the first

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "gcp_project_id" {
+  description = "The name of the GCP Project where all resources will be launched."
+}
+
+variable "gcp_credentials" {
+  description = "The name of the GCP Project where all resources will be launched."
+}
+
+variable "gcp_region" {
+  description = "The region in which all GCP resources will be launched."
+}
+
+variable "keyring_name" {
+  description = "KeyRing Name."
+}
+
+variable "keyring_location" {
+  description = "KeyRing Name."
+}
