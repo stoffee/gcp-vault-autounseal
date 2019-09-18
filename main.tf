dd5bd8f0579e77bdb3f637ce48fccacd6e25c690
stoffee <chris@roadkill.org>
2019-09-18 13:33:23 -0700

    fixed keyring_name

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "google_compute_instance" "vault" {
 
     sudo echo -e '[Unit]\nDescription="HashiCorp Vault - A tool for managing secrets"\nDocumentation=https://www.vaultproject.io/docs/\nRequires=network-online.target\nAfter=network-online.target\n\n[Service]\nExecStart=/usr/bin/vault server -config=/opt/vault/config.hcl\nExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nKillSignal=SIGINT\nRestart=on-failure\nRestartSec=5\n\n[Install]\nWantedBy=multi-user.target\n' > /lib/systemd/system/vault.service
 
-    sudo echo -e 'storage "file" {\n  path = "/opt/vault"\n}\n\nlistener "tcp" {\n  address     = "127.0.0.1:8200"\n  tls_disable = 1\n}\n\nseal "gcpckms" {\n  project     = ${var.gcp_project_id}\n  region      = ${var.keyring_location}\n  key_ring    = ${var.keyring}\n  crypto_key  = ${var.crypto_key}\n}\n\ndisable_mlock = true\n' > /opt/vault/config.hcl
+    sudo echo -e 'storage "file" {\n  path = "/opt/vault"\n}\n\nlistener "tcp" {\n  address     = "127.0.0.1:8200"\n  tls_disable = 1\n}\n\nseal "gcpckms" {\n  project     = ${var.gcp_project_id}\n  region      = ${var.keyring_location}\n  key_ring    = ${var.keyring_name}\n  crypto_key  = ${var.crypto_key}\n}\n\ndisable_mlock = true\n' > /opt/vault/config.hcl
 
     sudo chmod 0664 /lib/systemd/system/vault.service
 
